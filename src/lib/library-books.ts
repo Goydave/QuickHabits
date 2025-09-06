6212354f69f44249a63bd70dfa09c6b8e485c516
Goy Dave <goydave45@gmail.com>
2025-09-06 13:59:13 +0000

    bro, i love the work and effort you are putting i in this, now please br

--- src/lib/library-books.ts
+++ src/lib/library-books.ts
@@ -359,5 +359,355 @@ export const LIBRARY_BOOKS: LibraryBook[] = [
         author: 'Fyodor Dostoevsky',
         coverUrl: 'https://picsum.photos/300/400?random=56',
         url: 'https://www.gutenberg.org/files/27827/27827-0.txt',
+    },
+    {
+        id: '6130',
+        title: 'The Iliad of Homer',
+        author: 'Homer, translated by Alexander Pope',
+        coverUrl: 'https://picsum.photos/300/400?random=57',
+        url: 'https://www.gutenberg.org/files/6130/6130-0.txt'
+    },
+    {
+        id: '1727',
+        title: 'The Odyssey',
+        author: 'Homer',
+        coverUrl: 'https://picsum.photos/300/400?random=58',
+        url: 'https://www.gutenberg.org/files/1727/1727-0.txt'
+    },
+    {
+        id: '2852',
+        title: 'The Hound of the Baskervilles',
+        author: 'Arthur Conan Doyle',
+        coverUrl: 'https://picsum.photos/300/400?random=59',
+        url: 'https://www.gutenberg.org/files/2852/2852-0.txt'
+    },
+    {
+        id: '205',
+        title: 'Walden, and On The Duty Of Civil Disobedience',
+        author: 'Henry David Thoreau',
+        coverUrl: 'https://picsum.photos/300/400?random=60',
+        url: 'https://www.gutenberg.org/files/205/205-0.txt'
+    },
+    {
+        id: '16328',
+        title: 'Beowulf',
+        author: 'Anonymous',
+        coverUrl: 'https://picsum.photos/300/400?random=61',
+        url: 'https://www.gutenberg.org/files/16328/16328-0.txt'
+    },
+    {
+        id: '1064',
+        title: 'The Canterbury Tales, and Other Poems',
+        author: 'Geoffrey Chaucer',
+        coverUrl: 'https://picsum.photos/300/400?random=62',
+        url: 'https://www.gutenberg.org/files/1064/1064-0.txt'
+    },
+    {
+        id: '158',
+        title: 'Emma',
+        author: 'Jane Austen',
+        coverUrl: 'https://picsum.photos/300/400?random=63',
+        url: 'https://www.gutenberg.org/files/158/158-0.txt'
+    },
+    {
+        id: '946',
+        title: 'David Copperfield',
+        author: 'Charles Dickens',
+        coverUrl: 'https://picsum.photos/300/400?random=64',
+        url: 'https://www.gutenberg.org/files/946/946-0.txt'
+    },
+    {
+        id: '14264',
+        title: 'The Jungle Book',
+        author: 'Rudyard Kipling',
+        coverUrl: 'https://picsum.photos/300/400?random=65',
+        url: 'https://www.gutenberg.org/files/14264/14264-0.txt'
+    },
+    {
+        id: '244',
+        title: 'A Study in Scarlet',
+        author: 'Arthur Conan Doyle',
+        coverUrl: 'https://picsum.photos/300/400?random=66',
+        url: 'https://www.gutenberg.org/files/244/244-0.txt'
+    },
+    {
+        id: '16389',
+        title: 'The Memoirs of Sherlock Holmes',
+        author: 'Arthur Conan Doyle',
+        coverUrl: 'https://picsum.photos/300/400?random=67',
+        url: 'https://www.gutenberg.org/files/16389/16389-0.txt'
+    },
+    {
+        id: '8800',
+        title: 'The Return of Sherlock Holmes',
+        author: 'Arthur Conan Doyle',
+        coverUrl: 'https://picsum.photos/300/400?random=68',
+        url: 'https://www.gutenberg.org/files/8800/8800-0.txt'
+    },
+    {
+        id: '108',
+        title: 'The Sign of the Four',
+        author: 'Arthur Conan Doyle',
+        coverUrl: 'https://picsum.photos/300/400?random=69',
+        url: 'https://www.gutenberg.org/files/108/108-0.txt'
+    },
+    {
+        id: '23',
+        title: 'The Narrative of the Life of Frederick Douglass',
+        author: 'Frederick Douglass',
+        coverUrl: 'https://picsum.photos/300/400?random=70',
+        url: 'https://www.gutenberg.org/files/23/23-0.txt'
+    },
+    {
+        id: '514',
+        title: 'Little Women',
+        author: 'Louisa May Alcott',
+        coverUrl: 'https://picsum.photos/300/400?random=71',
+        url: 'https://www.gutenberg.org/files/514/514-0.txt'
+    },
+    {
+        id: '2641',
+        title: 'A Room with a View',
+        author: 'E. M. Forster',
+        coverUrl: 'https://picsum.photos/300/400?random=72',
+        url: 'https://www.gutenberg.org/files/2641/2641-0.txt'
+    },
+    {
+        id: '394',
+        title: 'Cranford',
+        author: 'Elizabeth Gaskell',
+        coverUrl: 'https://picsum.photos/300/400?random=73',
+        url: 'https://www.gutenberg.org/files/394/394-0.txt'
+    },
+    {
+        id: '4085',
+        title: 'The Age of Innocence',
+        author: 'Edith Wharton',
+        coverUrl: 'https://picsum.photos/300/400?random=74',
+        url: 'https://www.gutenberg.org/files/4085/4085-0.txt'
+    },
+    {
+        id: '37106',
+        title: 'The Secret Garden',
+        author: 'Frances Hodgson Burnett',
+        coverUrl: 'https://picsum.photos/300/400?random=75',
+        url: 'https://www.gutenberg.org/files/37106/37106-0.txt'
+    },
+    {
+        id: '2591',
+        title: 'The House of Mirth',
+        author: 'Edith Wharton',
+        coverUrl: 'https://picsum.photos/300/400?random=76',
+        url: 'https://www.gutenberg.org/files/2591/2591-0.txt'
+    },
+    {
+        id: '19337',
+        title: 'The Wind in the Willows',
+        author: 'Kenneth Grahame',
+        coverUrl: 'https://picsum.photos/300/400?random=77',
+        url: 'https://www.gutenberg.org/files/19337/19337-0.txt'
+    },
+    {
+        id: '2554',
+        title: 'Crime and Punishment',
+        author: 'Fyodor Dostoevsky',
+        coverUrl: 'https://picsum.photos/300/400?random=78',
+        url: 'https://www.gutenberg.org/files/2554/2554-0.txt'
+    },
+    {
+        id: '61533',
+        title: 'The Idiot',
+        author: 'Fyodor Dostoevsky',
+        coverUrl: 'https://picsum.photos/300/400?random=79',
+        url: 'https://www.gutenberg.org/files/61533/61533-0.txt'
+    },
+    {
+        id: '600',
+        title: 'Notes from the Underground',
+        author: 'Fyodor Dostoevsky',
+        coverUrl: 'https://picsum.photos/300/400?random=80',
+        url: 'https://www.gutenberg.org/files/600/600-0.txt'
+    },
+    {
+        id: '1250',
+        title: 'Madame Bovary',
+        author: 'Gustave Flaubert',
+        coverUrl: 'https://picsum.photos/300/400?random=81',
+        url: 'https://www.gutenberg.org/files/1250/1250-0.txt'
+    },
+    {
+        id: '161',
+        title: 'Sense and Sensibility',
+        author: 'Jane Austen',
+        coverUrl: 'https://picsum.photos/300/400?random=82',
+        url: 'https://www.gutenberg.org/files/161/161-0.txt'
+    },
+    {
+        id: '105',
+        title: 'Persuasion',
+        author: 'Jane Austen',
+        coverUrl: 'https://picsum.photos/300/400?random=83',
+        url: 'https://www.gutenberg.org/files/105/105-0.txt'
+    },
+    {
+        id: '141',
+        title: 'Mansfield Park',
+        author: 'Jane Austen',
+        coverUrl: 'https://picsum.photos/300/400?random=84',
+        url: 'https://www.gutenberg.org/files/141/141-0.txt'
+    },
+    {
+        id: '1212',
+        title: 'Northanger Abbey',
+        author: 'Jane Austen',
+        coverUrl: 'https://picsum.photos/300/400?random=85',
+        url: 'https://www.gutenberg.org/files/1212/1212-0.txt'
+    },
+    {
+        id: '20228',
+        title: 'The Red Badge of Courage',
+        author: 'Stephen Crane',
+        coverUrl: 'https://picsum.photos/300/400?random=86',
+        url: 'https://www.gutenberg.org/files/20228/20228-0.txt'
+    },
+    {
+        id: '42',
+        title: 'The Merry Adventures of Robin Hood',
+        author: 'Howard Pyle',
+        coverUrl: 'https://picsum.photos/300/400?random=87',
+        url: 'https://www.gutenberg.org/files/42/42-0.txt'
+    },
+    {
+        id: '41',
+        title: 'The Legend of Sleepy Hollow',
+        author: 'Washington Irving',
+        coverUrl: 'https://picsum.photos/300/400?random=88',
+        url: 'https://www.gutenberg.org/files/41/41-0.txt'
+    },
+    {
+        id: '20',
+        title: 'Paradise Lost',
+        author: 'John Milton',
+        coverUrl: 'https://picsum.photos/300/400?random=89',
+        url: 'https://www.gutenberg.org/files/20/20-0.txt'
+    },
+    {
+        id: '33',
+        title: 'The Scarlet Pimpernel',
+        author: 'Baroness Orczy',
+        coverUrl: 'https://picsum.photos/300/400?random=90',
+        url: 'https://www.gutenberg.org/files/33/33-0.txt'
+    },
+    {
+        id: '82',
+        title: 'Ivanhoe',
+        author: 'Walter Scott',
+        coverUrl: 'https://picsum.photos/300/400?random=91',
+        url: 'https://www.gutenberg.org/files/82/82-0.txt'
+    },
+    {
+        id: '2705',
+        title: 'The Last of the Mohicans',
+        author: 'James Fenimore Cooper',
+        coverUrl: 'https://picsum.photos/300/400?random=92',
+        url: 'https://www.gutenberg.org/files/2705/2705-0.txt'
+    },
+    {
+        id: '1837',
+        title: 'The Last Man',
+        author: 'Mary Shelley',
+        coverUrl: 'https://picsum.photos/300/400?random=93',
+        url: 'https://www.gutenberg.org/files/1837/1837-0.txt'
+    },
+    {
+        id: '521',
+        title: 'The Life and Adventures of Robinson Crusoe',
+        author: 'Daniel Defoe',
+        coverUrl: 'https://picsum.photos/300/400?random=94',
+        url: 'https://www.gutenberg.org/files/521/521-0.txt'
+    },
+    {
+        id: '3825',
+        title: 'Pygmalion',
+        author: 'George Bernard Shaw',
+        coverUrl: 'https://picsum.photos/300/400?random=95',
+        url: 'https://www.gutenberg.org/files/3825/3825-0.txt'
+    },
+    {
+        id: '1155',
+        title: 'The Importance of Being Earnest',
+        author: 'Oscar Wilde',
+        coverUrl: 'https://picsum.photos/300/400?random=96',
+        url: 'https://www.gutenberg.org/files/1155/1155-0.txt'
+    },
+    {
+        id: '245',
+        title: 'The Man Who Was Thursday',
+        author: 'G.K. Chesterton',
+        coverUrl: 'https://picsum.photos/300/400?random=97',
+        url: 'https://www.gutenberg.org/files/245/245-0.txt'
+    },
+    {
+        id: '1322',
+        title: 'Leaves of Grass',
+        author: 'Walt Whitman',
+        coverUrl: 'https://picsum.photos/300/400?random=98',
+        url: 'https://www.gutenberg.org/files/1322/1322-0.txt'
+    },
+    {
+        id: '19942',
+        title: 'The Divine Comedy',
+        author: 'Dante Alighieri',
+        coverUrl: 'https://picsum.photos/300/400?random=99',
+        url: 'https://www.gutenberg.org/files/19942/19942-0.txt'
+    },
+    {
+        id: '1513',
+        title: 'Romeo and Juliet',
+        author: 'William Shakespeare',
+        coverUrl: 'https://picsum.photos/300/400?random=100',
+        url: 'https://www.gutenberg.org/files/1513/1513-0.txt'
+    },
+    {
+        id: '1533',
+        title: 'Hamlet',
+        author: 'William Shakespeare',
+        coverUrl: 'https://picsum.photos/300/400?random=101',
+        url: 'https://www.gutenberg.org/files/1533/1533-0.txt'
+    },
+    {
+        id: '1524',
+        title: 'Macbeth',
+        author: 'William Shakespeare',
+        coverUrl: 'https://picsum.photos/300/400?random=102',
+        url: 'https://www.gutenberg.org/files/1524/1524-0.txt'
+    },
+    {
+        id: '2265',
+        title: 'Othello',
+        author: 'William Shakespeare',
+        coverUrl: 'https://picsum.photos/300/400?random=103',
+        url: 'https://www.gutenberg.org/files/2265/2265-0.txt'
+    },
+    {
+        id: '2267',
+        title: 'The Tempest',
+        author: 'William Shakespeare',
+        coverUrl: 'https://picsum.photos/300/400?random=104',
+        url: 'https://www.gutenberg.org/files/2267/2267-0.txt'
+    },
+    {
+        id: '1795',
+        title: 'A Midsummer Night\'s Dream',
+        author: 'William Shakespeare',
+        coverUrl: 'https://picsum.photos/300/400?random=105',
+        url: 'https://www.gutenberg.org/files/1795/1795-0.txt'
+    },
+    {
+        id: '2264',
+        title: 'The Taming of the Shrew',
+        author: 'William Shakespeare',
+        coverUrl: 'https://picsum.photos/300/400?random=106',
+        url: 'https://www.gutenberg.org/files/2264/2264-0.txt'
     }
 ];
